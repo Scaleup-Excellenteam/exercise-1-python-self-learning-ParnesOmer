56f02e2013cb36dc17b647fdea24447f631f4d84
ParnesOmer <omer.parnes123@gmail.com>
2025-03-30 19:12:02 +0300

    Update 5.4.communicating_vessels.py

--- src/5.4.communicating_vessels.py
+++ src/5.4.communicating_vessels.py
@@ -1,17 +1,16 @@
 def generator_interleave(*iterables):
     """
-    Intertwine elements from multiple iterables using a generator.
-    This function takes one or more iterable objects and returns a generator
-    that yields their elements in an interleaved fashion.
-
-    Parameters:
-    *iterables: One or more iterable objects (e.g., lists, tuples, strings).
-
-    Yields:
-    The next interleaved element from the input iterables, one at a time.
-    """
+       Intertwine iterators elements from multiple iterables.
+       This function takes one or more iterable objects and returns a generator
+       that yields their elements Intertwine. For example, given iterables
+       Parameters:
+       *iterables: One or more iterable objects (e.g., lists, tuples, strings).
+       Yields:
+       The next interleaved element from the input iterables.
+       """
     # Convert each iterable to an iterator
     iterators = [iter(it) for it in iterables]
+
     # Continue until there are no more iterators
     while iterators:
         # Iterate over each iterator
@@ -22,16 +21,18 @@ def generator_interleave(*iterables):
             except StopIteration:
                 # If the iterator is exhausted, remove it from the list
                 iterators.remove(it)
-def interleave(*iterables):
-     """
-    Intertwine elements from multiple iterables and return a list.
-   
-    Parameters:
-    *iterables: One or more iterable objects (e.g., lists, tuples, strings).
 
-    Returns:
-    A list of interleaved elements from the input iterables.
+
+def interleave(*iterables):
     """
+       Intertwine iterators elements from multiple iterables.
+       This function takes one or more iterable objects and returns a generator
+       that yields their elements Intertwine. For example, given iterables
+       Parameters:
+       *iterables: One or more iterable objects (e.g., lists, tuples, strings).
+       Yields:
+       The next interleaved element from the input iterables.
+       """
     # Convert each iterable to an iterator
     iterators = [iter(it) for it in iterables]
     final_list = []
@@ -40,12 +41,13 @@ def interleave(*iterables):
         # Iterate over each iterator
         for it in iterators:
             try:
-                # Append the next item from the iterator
+                # Yield the next item from the iterator
                 final_list.append(next(it))
             except StopIteration:
                 # If the iterator is exhausted, remove it from the list
                 iterators.remove(it)
     return final_list
+
 
 if __name__ == '__main__':
     result = list(generator_interleave('abc', [1, 2, 3], ('!', '@', '#')))
