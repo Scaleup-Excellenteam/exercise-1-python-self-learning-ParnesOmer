8878b0725a0f595456efcb877797474825853c34
ParnesOmer <omer.parnes123@gmail.com>
2025-03-30 22:23:39 +0300

    Update 6.4.remember_remember.py

--- src/6.4.remember_remember.py
+++ src/6.4.remember_remember.py
@@ -1,19 +1,39 @@
-# remember_remember
+"""
+Module for reading an image and extracting an encrypted message embedded
+using a specific encoding scheme. It retrieves characters from pixel data.
+"""
 from PIL import Image
+
+
 def remember_remember(image_name):
+    """
+        Extracts an encrypted message embedded in an image.
+
+        This function opens an image file, iterates through its pixels, and attempts
+        to decode a hidden message based on pixel values. The function looks for a
+        pixel value of 1 to extract corresponding characters.
+
+        Parameters:
+        image_name (str): The file path to the image containing the hidden message.
+
+        Returns:
+        str: The decrypted message embedded in the image.
+        """
     try:
         with Image.open(image_name) as image:
-            Encrypted_message = []
+            encrypted_message = []
             width, height = image.size
             for w in range(width):
                 for h in range(height):
                     pixel = image.getpixel((w, h))
                     if pixel == 1:
-                        Encrypted_message.append(chr(h))
+                        encrypted_message.append(chr(h))
                         break
-            return ''.join(Encrypted_message)
+            return ''.join(encrypted_message)
     except FileNotFoundError:
         print(f"File not found: {image_name}")
+        return ""  # Ensures that the function always returns a value
+
 
 if __name__ == "__main__":
     print(remember_remember("code.png"))
