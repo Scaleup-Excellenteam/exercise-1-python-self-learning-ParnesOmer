3701b4b3621dab051234ecf246c99037d630d72a
ParnesOmer <omer.parnes123@gmail.com>
2025-03-30 18:45:33 +0300

    Update 5.4.vessels_communicating.py

--- src/5.4.vessels_communicating.py
+++ src/5.4.vessels_communicating.py
@@ -1,13 +1,15 @@
-def vessels_communicating(*iterables):
+def generator_interleave(*iterables):
     """
-       Intertwine iterators elements from multiple iterables.
-       This function takes one or more iterable objects and returns a generator
-       that yields their elements Intertwine. For example, given iterables
-       Parameters:
-       *iterables: One or more iterable objects (e.g., lists, tuples, strings).
-       Yields:
-       The next interleaved element from the input iterables.
-       """
+    Intertwine elements from multiple iterables using a generator.
+    This function takes one or more iterable objects and returns a generator
+    that yields their elements in an interleaved fashion.
+
+    Parameters:
+    *iterables: One or more iterable objects (e.g., lists, tuples, strings).
+
+    Yields:
+    The next interleaved element from the input iterables, one at a time.
+    """
     # Convert each iterable to an iterator
     iterators = [iter(it) for it in iterables]
 
@@ -21,8 +23,33 @@ def vessels_communicating(*iterables):
             except StopIteration:
                 # If the iterator is exhausted, remove it from the list
                 iterators.remove(it)
+def interleave(*iterables):
+     """
+    Intertwine elements from multiple iterables and return a list.
+   
+    Parameters:
+    *iterables: One or more iterable objects (e.g., lists, tuples, strings).
 
+    Returns:
+    A list of interleaved elements from the input iterables.
+    """
+    # Convert each iterable to an iterator
+    iterators = [iter(it) for it in iterables]
+    final_list = []
+    # Continue until there are no more iterators
+    while iterators:
+        # Iterate over each iterator
+        for it in iterators:
+            try:
+                # Append the next item from the iterator
+                final_list.append(next(it))
+            except StopIteration:
+                # If the iterator is exhausted, remove it from the list
+                iterators.remove(it)
+    return final_list
 
 if __name__ == '__main__':
-    result = list(vessels_communicating('abc', [1, 2, 3], ('!', '@', '#')))
+    result = list(generator_interleave('abc', [1, 2, 3], ('!', '@', '#')))
+    result2 = interleave('abc', [1, 2, 3], ('!', '@', '#'))
     print(result)
+    print(result2)
