29e7a8c88c589fa241bccdb1d1dc36d4fbf45919
ParnesOmer <omer.parnes123@gmail.com>
2025-03-30 21:35:48 +0300

    Update 5.3.parsle_tongue.py

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -1,23 +1,32 @@
-def parsle_tongue(filename='logo.jpg', chunk_size=1024):
+"""
+Module to read a binary file and extract sequences of lowercase alphabetical characters
+ending with '!' character. The sequences must be at least 5 characters long.
+
+This module contains the function `parsle_tongue` which reads a file in chunks and yields
+sequences of valid characters as described above.
+
+Raises:
+FileNotFoundError: If the specified file is not found.
+"""
+
+
+def parsle_tongue():
     """
         Reads a binary file in chunks and yields sequences of lowercase alphabetical
         characters that end with the '!' character, the
         sequence is at least 5 characters long.
 
-        Parameters:
-        filename (str): The path to the file to be read.
-        chunk_size (int): The size of the chunks to read at a time (default is 1024 bytes).
-
         Yields:
         str: A sequence of lowercase alphabetical characters followed by a '!' character.
 
         Raises:
-        TypeError('filename cannot be None')
         FileNotFoundError: If the specified file is not found.
         """
     buffer = b''  # Buffer to hold the current chunk and last characters
     min_size = 5
+    chunk_size = 1024
     current_string = ""
+    filename = 'logo.jpg'
     try:
         with open(filename, 'rb') as file:
             while True:
@@ -43,7 +52,6 @@ def parsle_tongue(filename='logo.jpg', chunk_size=1024):
 
 if __name__ == "__main__":
     # Using a generator to read the file in parts
-    filename = 'logo.jpg'
-    gen = parsle_tongue(filename)
+    gen = parsle_tongue()
     for word in gen:
         print(word)
