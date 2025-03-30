b8fc67b4bf7afcf14e86ffe1d726b5a13c83d95e
ParnesOmer <omer.parnes123@gmail.com>
2025-03-30 20:21:27 +0300

    Update 5.3.parsle_tongue.py

--- src/5.3.parsle_tongue.py
+++ src/5.3.parsle_tongue.py
@@ -1,5 +1,23 @@
 def parsle_tongue(filename, chunk_size=1024):
-    buffer = b''
+    """
+        Reads a binary file in chunks and yields sequences of lowercase alphabetical
+        characters that end with the '!' character, the
+        sequence is at least 5 characters long.
+
+        Parameters:
+        filename (str): The path to the file to be read.
+        chunk_size (int): The size of the chunks to read at a time (default is 1024 bytes).
+
+        Yields:
+        str: A sequence of lowercase alphabetical characters followed by a '!' character.
+
+        Raises:
+        TypeError('filename cannot be None')
+        FileNotFoundError: If the specified file is not found.
+        """
+    if filename is None:
+        raise TypeError('filename cannot be None')
+    buffer = b''  # Buffer to hold the current chunk and last characters
     min_size = 5
     current_string = ""
     try:
@@ -8,25 +26,22 @@ def parsle_tongue(filename, chunk_size=1024):
                 chunk = file.read(chunk_size)
                 if not chunk:
                     break
-
                 buffer += chunk
-                for char in chunk:
+                for char in buffer:
                     char = chr(char)
                     if char.isalpha() and char.islower() and char.isascii():
                         current_string += char
                     else:
                         if char == '!' and len(current_string) >= min_size:
-                            current_string += char
                             yield current_string
                         current_string = ""
-
-                    # שמור את חמשת הבתים האחרונים כדי לוודא שמחרוזת לא תתפצל בין נתחים
-                    # buffer = buffer[-min_size:]
+                # This line ensures that only the last 'min_size' characters are kept in the buffer
+                # so that strings don't get split between chunks.
+                buffer = buffer[-min_size:]
 
     except FileNotFoundError:
         print("The file was not found.")
-    # except OSError:
-    #     print("An error occurred while reading the file.")
+
 
 if __name__ == "__main__":
     # Using a generator to read the file in parts
